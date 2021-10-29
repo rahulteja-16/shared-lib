e8fef4e05d5e56abf6fada7bd3dd04d2215e4273
Rahul Teja <rahulteja.a@hotmail.com>
2021-10-29 23:31:14 +0530

    Boop Component

---
+++ src/Components/Boop/index.tsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+import { useSpring, animated } from 'react-spring'
+
+const Boop = ({
+	x = 0,
+	y = 0,
+	rotation = 0,
+	scale = 1,
+	timing = 150,
+	children,
+}) => {
+	const [isBooped, setIsBooped] = useState(false)
+
+	const style = useSpring({
+		display: 'inline-block',
+		backfaceVisibility: 'hidden',
+		transform: isBooped
+			? `translate(${x}px, ${y}px)
+                rotate(${rotation}deg)
+                scale(${scale})`
+			: `translate(0px, 0px)
+                rotate(0deg)
+                scale(1)`,
+		config: {
+			tension: 300,
+			friction: 10,
+		},
+	})
+	useEffect(() => {
+		if (!isBooped) {
+			return
+		}
+
+		const timeoutId = window.setTimeout(() => {
+			setIsBooped(false)
+		}, timing)
+
+		return () => {
+			window.clearTimeout(timeoutId)
+		}
+	}, [isBooped, timing])
+
+	const trigger = () => {
+		setIsBooped(true)
+	}
+
+	return (
+		<animated.span onMouseEnter={trigger} style={style}>
+			{children}
+		</animated.span>
+	)
+}
+
+export default Boop
