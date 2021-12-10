1e239f1cc77a2aeed1afa99102bccde807fad0a4
Rahul Teja <rahulteja.a@hotmail.com>
2021-12-10 17:42:31 +0530

    Color Changes

--- src/Components/DropDown/styles.tsx
+++ src/Components/DropDown/styles.tsx
@@ -1,22 +1,22 @@
 import styled from 'styled-components'
 
 export const LabelWrapper = styled.label`
-	color: var(--text-100);
+	color: var(--gray-400);
 	font-size: var(--fs-sm);
 	font-weight: var(--fw-md);
 	text-transform: capitalize;
-	font-family: var(--font);
+	font-family: var(--font-sans);
 	letter-spacing: 0.5px;
 `
 
 export const SelectWrapper = styled.select`
 	font-size: var(--fs-md);
 	border: none;
-	border-bottom: 1px solid var(--blue-40);
+	border-bottom: 1px solid var(--gray-300);
 	padding-bottom: 5px;
 	padding-top: 5px;
-	background-color: var(--bg-blue);
-	color: var(--text-200);
+	background-color: transparent;
+	color: var(--gray-200);
 	&:focus {
 		outline: thin dotted;
 		color: var(--blue-20);
